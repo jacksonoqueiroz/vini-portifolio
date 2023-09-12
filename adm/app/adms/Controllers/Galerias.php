89b73ec5532b7545012215107cb7f0c1032cb4a9
jacksonoqueiroz <40044470+jacksonoqueiroz@users.noreply.github.com>
2023-09-12 20:11:56 -0300

    Update Galerias.php

--- adm/app/adms/Controllers/Galerias.php
+++ adm/app/adms/Controllers/Galerias.php
@@ -13,14 +13,31 @@ if(!defined('R4F5CC')){
 class Galerias
 {
 	private $dados;
+	private $dadosForm;
 
 	public function index()	{
+		$this->dadosForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+		if (!empty($this->dadosForm['EditGaleriaHome'])) {
+			$editGaleria = new \App\adms\Models\AdmsViewGalerias();
+			if($editGaleria->editGalerias($this->dadosForm)){
+				$urlDestino = URLADM . "view" . "#galerias";
+				header("Location: $urlDestino");
+			}else{
+				$this->dados['galerias'] = $this->dadosForm;
+				$this->viewGalerias();
+			}
+		}else{
+			$this->dadosGalerias();
+			$this->viewGalerias();	
+		}	
+				
+	}
+	private function dadosGalerias(){
 		$viewGalerias = new \App\adms\Models\AdmsViewGalerias();
 		$this->dados['galerias'] = $viewGalerias->viewGalerias();
-		echo "<pre>";
-		var_dump($this->dados);
-		echo "</pre>";
-		
+	}
+
+	private function viewGalerias(){
 		$carregarView = new \Core\ConfigView("adms/views/pgHome/editPgHomeGalerias",$this->dados);
 		$carregarView->renderizar();
 	}
