a3ef880f25d294943fc622b871e62ed5dbb125e4
jacksonoqueiroz <40044470+jacksonoqueiroz@users.noreply.github.com>
2023-09-26 20:11:31 -0300

    Add files via upload

---
+++ adm/app/adms/Models/AdmsViewGalprof2.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\adms\Models;
+
+if(!defined('R4F5CC')){
+    header("Location: /");    
+    die("Erro: Página não encontrada!");
+}
+
+use PDO;
+
+/**
+ * Class AdmsViewGalprof1
+ */
+class AdmsViewGalprof2 extends Conn
+{
+	private object $conn;
+	private $dados;
+
+	public function viewGaleriaProf(){
+			$this->conn = $this->connect();
+			$query_galeria_profissional = "SELECT id, img_gal_prof_1, img_gal_prof_2, img_gal_prof_3, img_gal_prof_4, img_gal_prof_5, img_gal_prof_6 FROM galeria_profissional";
+			$result_galeria_profissional = $this->conn->prepare($query_galeria_profissional);
+			$result_galeria_profissional->execute();
+			$this->dados = $result_galeria_profissional->fetch();
+			return $this->dados;
+	}
+	public function editGaleriaProf($dados)
+	{
+		$this->dados = $dados;
+		$this->conn = $this->connect();
+		$query_gal2 = "UPDATE galeria_profissional SET img_gal_prof_2=:img_gal_prof_2, modified=NOW() WHERE id=:id";
+		$edit_gal1 = $this->conn->prepare($query_gal2);
+		$edit_gal1->bindParam(':img_gal_prof_2', $this->dados['img_nova']['name']);
+		$edit_gal1->bindParam(':id', $this->dados['id'], PDO::PARAM_INT);
+		$edit_gal1->execute();
+
+		if($edit_gal1->rowCount()) {
+			if ($this->upload()) {
+				$_SESSION['msg_galerias'] = '<div class="alert alert-success" role="alert">A imagem 2 da galeria profissional foi editado!</div>';
+				return true;
+			}else{
+				$_SESSION['msg_galerias'] = '<div class="alert alert-danger" role="alert"A imagem 2 da galeria profissional NÃO foi editado!</div>';
+				return false;
+			}
+			
+		}else{
+			$_SESSION['msg_galerias'] = '<div class="alert alert-danger" role="alert"A imagem 2 da galeria profissional NÃO foi editado!</div>';
+			return false;
+		}
+
+	}
+	private function upload(){
+		//Diretório onde o arquivo será salvo
+		$diretorio = "app/adms/assets/images/galeria/";
+
+		if (move_uploaded_file($this->dados['img_nova']['tmp_name'], $diretorio . $this->dados['img_nova']['name'])) {
+			$this->apagarImagem();
+			return true;
+		}else{
+			return false;
+		}
+	}
+	private function apagarImagem(){
+		$img_antiga = "app/adms/assets/images/galeria/" . $this->dados['img_gal_prof_2'];
+		unlink($img_antiga);
+	}
+}
---
+++ adm/app/adms/Models/AdmsViewGalprof6.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\adms\Models;
+
+if(!defined('R4F5CC')){
+    header("Location: /");    
+    die("Erro: Página não encontrada!");
+}
+
+use PDO;
+
+/**
+ * Class AdmsViewGalprof6
+ */
+class AdmsViewGalprof6 extends Conn
+{
+	private object $conn;
+	private $dados;
+
+	public function viewGaleriaProf(){
+			$this->conn = $this->connect();
+			$query_galeria_profissional = "SELECT id, img_gal_prof_1, img_gal_prof_2, img_gal_prof_3, img_gal_prof_4, img_gal_prof_5, img_gal_prof_6 FROM galeria_profissional";
+			$result_galeria_profissional = $this->conn->prepare($query_galeria_profissional);
+			$result_galeria_profissional->execute();
+			$this->dados = $result_galeria_profissional->fetch();
+			return $this->dados;
+	}
+	public function editGaleriaProf($dados)
+	{
+		$this->dados = $dados;
+		$this->conn = $this->connect();
+		$query_gal6 = "UPDATE galeria_profissional SET img_gal_prof_6=:img_gal_prof_6, modified=NOW() WHERE id=:id";
+		$edit_gal6 = $this->conn->prepare($query_gal6);
+		$edit_gal6->bindParam(':img_gal_prof_6', $this->dados['img_nova']['name']);
+		$edit_gal6->bindParam(':id', $this->dados['id'], PDO::PARAM_INT);
+		$edit_gal6->execute();
+
+		if($edit_gal6->rowCount()) {
+			if ($this->upload()) {
+				$_SESSION['msg_galerias'] = '<div class="alert alert-success" role="alert">A imagem 6 da galeria profissional foi editado!</div>';
+				return true;
+			}else{
+				$_SESSION['msg_galerias'] = '<div class="alert alert-danger" role="alert"A imagem 6 da galeria profissional NÃO foi editado!</div>';
+				return false;
+			}
+			
+		}else{
+			$_SESSION['msg_galerias'] = '<div class="alert alert-danger" role="alert"A imagem 6 da galeria profissional NÃO foi editado!</div>';
+			return false;
+		}
+
+	}
+	private function upload(){
+		//Diretório onde o arquivo será salvo
+		$diretorio = "app/adms/assets/images/galeria/";
+
+		if (move_uploaded_file($this->dados['img_nova']['tmp_name'], $diretorio . $this->dados['img_nova']['name'])) {
+			$this->apagarImagem();
+			return true;
+		}else{
+			return false;
+		}
+	}
+	private function apagarImagem(){
+		$img_antiga = "app/adms/assets/images/galeria/" . $this->dados['img_gal_prof_6'];
+		unlink($img_antiga);
+	}
+}
